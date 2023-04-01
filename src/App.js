fb108d95162c3fd34a13eb297bd9cf7ab8726eb2
dhatrishdixit <dhatrish29@gmail.com>
2023-04-01 17:41:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,75 +1,22 @@
-import React from 'react';
-import { useEffect,useState } from 'react';
-import './App.css';
-import SearchIcon from './search.svg'
-import MovieCard from './MovieCard';
+import React from "react";
+import Home from "./components/Home";
+import Summary from "./components/Summary";
+import './App.css'
+import { BrowserRouter,Route,Routes,Link, useParams } from "react-router-dom";
+const App = () =>{
+    return(
+        <BrowserRouter>
+   <div>
+    <Routes>
+    <Route path ="/" exact element ={<Home />} />
+    <Route path ="/:thetvdb" exact element ={<Summary />} />
+    </Routes>
+    
+    </div>
+    
+    </BrowserRouter>
 
-//90d0640d
-
-const API_URL = 'http://www.omdbapi.com?apikey=90d0640d' ;
-// const movie1 = 
-//       {
-//         "Title": "Amazing Spiderman Syndrome",
-//         "Year": "2012",
-//         "imdbID": "tt2586634",
-//         "Type": "movie",
-//         "Poster": "N/A"
-//       }
-   
- 
-const App = () => {
-    const [movies,setMovies] = useState([]);
-    const [searchTerm,setSearchTerm] = useState(['']);
-    const searchMovies = async(title) =>{
-        const response = await fetch(`${API_URL}&s=${title}`);
-        const data = await response.json();
-
-        setMovies(data.Search);
-    }
-    useEffect(()=>{
-            searchMovies('spider man')
-    },[]);
-    return(<>
-        <div className="app"><h1>Moviepidia</h1>
-
-        <div className="search">
-            <input 
-        value={searchTerm}
-        onChange={(e)=>setSearchTerm(e.target.value)} placeholder="search for movies"
-        onKeyUp={
-            (e)=>{
-                if (e.key == "Enter") {
-                    searchMovies(searchTerm)
-                }
-            }
-        }
-        />
-        <img
-        src={SearchIcon}
-       alt="search" 
-       onClick={() =>searchMovies(searchTerm)}
-        />
-        
-        </div>
-{
-    movies?.length>0?(
-           <div className="container">
-             {
-                movies.map((movie) =>(
-                    <MovieCard movie={movie}/>
-                ))
-             }
-           </div>
-           ):(
-           <div className ="empty">
-           <h2>No Movies found</h2>
-           </div>
-        )
-}
-        
-        </div>
-        </>
-    );
+    )
 }
 
 export default App;
---
+++ src/components/Summary.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+const API_URL = 'https://api.tvmaze.com/lookup/shows?thetvdb=';
+
+const Summary = () => {
+  const { thetvdb } = useParams();
+  const [data, setData] = useState({});
+  function stripHtml(html) {
+    var doc = new DOMParser().parseFromString(html, 'text/html');
+    return doc.body.textContent || "";
+  }
+  useEffect(() => {
+    fetch(API_URL + thetvdb)
+      .then((result) => result.json())
+      .then((data) => setData(data));
+  }, [thetvdb]);
+console.log(data)
+  return (
+    <div className='summary'>
+      <div className='app'>
+        {data.name ?<h1>{data.name}</h1>:<p>loading</p>}
+      </div>
+      <div className='content'> 
+      {data.image ? <img src={data.image.medium} alt={data.name} /> : <p>Loading...</p>}
+
+      <div className='written'>
+      <div className='runtime'>
+        Runtime - {data.runtime} hrs
+      </div>
+      <div className='story'>
+      {stripHtml(data.summary)}
+      </div></div></div>  
+      
+     <div className='submit'>
+     <button>
+      Book Show
+     </button>
+    </div>
+    </div>
+
+  );
+};
+
+export default Summary;
